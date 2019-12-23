d2534a7ea2ec22cc3f05575c34b600fa645b4b98
Gilmar Pupo <contact@gpupo.com>
2019-12-23 12:10:25 -0300

    src/

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -21,7 +21,7 @@ use Gpupo\CommonSdk\Console\AbstractApplication;
 use Gpupo\CommonSdk\FactoryInterface;
 use Gpupo\ShippingServices\Factory;
 use Psr\Log\LoggerInterface;
-use Psr\SimpleCache\CacheInterface;
+use Symfony\Contracts\Cache\CacheInterface;
 
 /**
  * @codeCoverageIgnore
